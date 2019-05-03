6ce258dc52837111a8fd6c884fb81ac02a4d97b0
Jon Kurinsky <jkurinsky@gmail.com>
2019-05-03 12:11:21 -0500

    set up socket stuff

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const constants = require('../../shared/constants');
 
 const App = () => {
-  return <div>Hello world from the app component!</div>;
+  const [socketId, setSocketId] = useState('');
+
+  useEffect(() => {
+    const socket = io();
+    socket.on(constants.MSG.CONNECT, () => {
+      console.log('connection id:', socket.id);
+      setSocketId(socket.id);
+    });
+  }, []);
+
+  return (
+    <div>
+      Hello world from the app component! This client's socket id is {socketId}{' '}
+    </div>
+  );
 };
 
 export default App;
