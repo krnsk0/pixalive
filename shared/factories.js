f798e987c0cb6fc66deafee584734e06ff978c90
Jon Kurinsky <jkurinsky@gmail.com>
2019-05-05 21:28:52 -0500

    adds more to factory functions

--- shared/factories.js
+++ shared/factories.js
@@ -1,6 +1,6 @@
 // some placeholder factory functions
 
-module.exports.spriteFactory = hash => {
+const spriteFactory = hash => {
   return {
     hash,
     users: {},
@@ -8,10 +8,46 @@ module.exports.spriteFactory = hash => {
   };
 };
 
-module.exports.userFactory = socketId => {
+const userFactory = socketId => {
   return {
     socketId,
+    selectedFrame: 0,
+    selectedLayer: 0,
+    selectedColor: { h: 0, s: 0, l: 0, o: 1.0 },
+    selectedTool: 'pen',
     x: null,
     y: null
   };
+};
+
+const frameFactory = () => {
+  return {
+    layers: []
+  };
+};
+
+const layerFactory = (w, h) => {
+  return {
+    name: 'new layer',
+    pixels: Array.from({ length: h }, () =>
+      Array.from({ length: w }, () => null)
+    )
+  };
+};
+
+const pixelFactory = (h = 0, s = 0, l = 0, o = 1.0) => {
+  return {
+    h,
+    s,
+    l,
+    o
+  };
+};
+
+module.exports = {
+  spriteFactory,
+  userFactory,
+  frameFactory,
+  layerFactory,
+  pixelFactory
 };
