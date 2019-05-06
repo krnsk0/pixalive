06332b415502cd5b0badf6fbd7081ae11469f275
Jon Kurinsky <jkurinsky@gmail.com>
2019-05-05 21:44:04 -0500

    deletes ununsed import

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import { ConnectionInfo, SingleLayer } from './';
 import { SocketContext, SpriteContext } from '../contexts';
