96fd900de1891d901c7b82a391730d4185f3e87d
Brendan Fallon <brendanc.fallon@gmail.com>
2019-05-08 15:45:19 -0500

    working on canvasClick function

---
+++ server/eventHandlers/canvasClick.js
@@ -0,0 +1,30 @@
+const constants = require('../../shared/constants');
+
+module.exports = (socket, namespacedIo, state, spriteHash, socketId) => {
+  // when a cursor moves...
+  socket.on(constants.MSG.CANVAS_CLICK, coords => {
+    // update their coords
+    const selectedColor = state[spriteHash].users[socketId].selectedColor;
+    const selectedTool = state[spriteHash].users[socketId].selectedTool;
+    const selectedFrame = state[spriteHash].users[socketId].selectedFrame
+    const selectedLayer = state[spriteHash].users[socketId].selectedLayer
+
+
+    //generates the list of changes to the image on server state
+    let changeList = []
+    if (selectedTool === constants.TOOLS.PEN){
+        changeList.push(
+            {
+                x: coords.x,
+                y: coords.y,
+                frameIdx: selectedFrame,
+                layerIdx: selectedLayer,
+                color: selectedColor
+            })
+    }
+    //takes list of changes, changes pixels
+    
+    // send only the cursor update
+    namespacedIo.emit(constants.MSG.SEND_SPRITE, state[spriteHash])
+  });
+};
