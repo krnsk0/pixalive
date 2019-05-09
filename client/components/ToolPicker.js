e5c7ad95c29d8b16b0dd7f7a3f5fb24951cc7979
Jon Kurinsky <jkurinsky@gmail.com>
2019-05-09 13:48:30 -0500

    fix missing bracket

--- client/components/ToolPicker.js
+++ client/components/ToolPicker.js
@@ -16,6 +16,7 @@ const ToolPicker = () => {
     if (sprite.users[socketId]) {
       selectedTool = sprite.users[socketId].selectedTool;
     }
+  }
 
   // click handler for selecting tool
   const onSelectToolClick = selectedTool => {
