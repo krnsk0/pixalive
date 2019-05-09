37c5dd35e661671cef890f8886ce8fab3e8b6636
Jon Kurinsky <jkurinsky@gmail.com>
2019-05-09 13:58:16 -0500

    adds server-side logic for deleting selected layers

--- server/eventHandlers/deleteSelectedLayer.js
+++ server/eventHandlers/deleteSelectedLayer.js
@@ -9,6 +9,15 @@ module.exports = (socket, namespacedIo, state, spriteHash, socketId) => {
     // get selected layer
     const selectedLayer = state[spriteHash].users[socketId].selectedLayer;
 
+    // delete layer
+    state[spriteHash].frames[selectedFrame].layers.splice(selectedLayer, 1);
+
+    // re-index layers
+    state[spriteHash].frames[selectedFrame].layers.forEach((layer, index) => {
+      layer.layerOrder = index;
+      return layer;
+    });
+
     //send updated sprite
     namespacedIo.emit(constants.MSG.SEND_SPRITE, state[spriteHash]);
   });
