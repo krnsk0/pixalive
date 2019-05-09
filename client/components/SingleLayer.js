a9a606d150b3f2f7a7e16f12f20d36f10ba8d637
Jon Kurinsky <jkurinsky@gmail.com>
2019-05-08 19:25:44 -0500

    clean up singlelayer and use refs to allow access to current prop values inside closures

--- client/components/SingleLayer.js
+++ client/components/SingleLayer.js
@@ -13,22 +13,19 @@ const throttle = require('../../shared/throttle');
 const SingleLayer = () => {
   const sprite = useContext(SpriteContext);
   const socket = useContext(SocketContext);
-
-  // set up a ref to the canvas element we'll render below
-  const canvasRef = useRef();
-
-  // set up component state for which pixel is selected
   const [screenCoords, setScreenCoords] = useState({
     x: false,
     y: false
   });
+  const screenCoordsRef = useRef();
+  screenCoordsRef.current = screenCoords; // allows accessing current coords in closed-over scopes
+  const spriteRef = useRef();
+  spriteRef.current = sprite; // allows accessing current sprite in closed-over scopes
+  const canvasRef = useRef(); // passed in to canvas below
 
-  // set up canvas on load
+  // set up canvas width an height after first mount
   useEffect(() => {
-    // store a reference to the canvas element itself
     const canvas = canvasRef.current;
-
-    // set its width and height
     canvas.width = constants.CANVAS_WIDTH;
     canvas.height = constants.CANVAS_HEIGHT;
     canvas.style.width = constants.CANVAS_WIDTH;
@@ -39,6 +36,17 @@ const SingleLayer = () => {
   useEffect(() => {
     // store a reference to the canvas element itself
     const canvas = canvasRef.current;
+
+    const onCanvasClick = () => {
+      const pixelCoords = convertScreenCoordsToPixelCoords(
+        screenCoordsRef.current,
+        spriteRef.current
+      );
+
+      if (socket) {
+        socket.emit(constants.MSG.CANVAS_CLICK, pixelCoords);
+      }
+    };
 
     // event listener helper
     const throttledOnMouseMove = throttle(evt => {
@@ -59,13 +67,13 @@ const SingleLayer = () => {
     // set up event listener for mouse movements
     if (socket) {
       canvas.addEventListener('mousemove', throttledOnMouseMove);
-      // canvas.addEventListener('click', onCanvasClick);
+      canvas.addEventListener('click', onCanvasClick);
     }
 
     // a callback to disable the canvas listener
     return () => {
       canvas.removeEventListener('mousemove', throttledOnMouseMove);
-      // canvas.removeEventListener('click', onCanvasClick);
+      canvas.removeEventListener('click', onCanvasClick);
     };
   }, [socket]);
 
@@ -74,45 +82,17 @@ const SingleLayer = () => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
 
-    // clear the canvas
+    // perform renders
     ctx.clearRect(0, 0, constants.CANVAS_WIDTH, constants.CANVAS_HEIGHT);
-
-    // draw the background
     renderBackdrop(ctx);
-
-    // draw pixels
     renderPixels(ctx, sprite, socket);
-
-    // draw selected pixel
     renderSelectedPixel(ctx, screenCoords, sprite);
-
-    // draw cursors
     renderCursors(ctx, sprite);
   });
 
-  const onCanvasClick = evt => {
-    // grab a current canvas ref
-    // get the relative coords of the mouse
-    const canvasRect = canvas.getBoundingClientRect();
-    const screenCoords = {
-      x: evt.clientX - canvasRect.left,
-      y: evt.clientY - canvasRect.top
-    };
-    console.log('screenCoords: ', screenCoords);
-    const pixelCoords = convertScreenCoordsToPixelCoords(screenCoords, sprite);
-
-    if (socket) {
-      socket.emit(constants.MSG.CANVAS_CLICK, pixelCoords);
-    }
-  };
-
   return (
     <div>
-      <canvas
-        id="canvas"
-        ref={canvasRef}
-        onClick={evt => onCanvasClick(evt, sprite)}
-      />
+      <canvas id="canvas" ref={canvasRef} />
     </div>
   );
 };
