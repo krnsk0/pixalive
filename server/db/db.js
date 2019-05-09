3e24283ecbd9e81ac98e639715ea963fd3500bc3
Brendan Fallon <brendanc.fallon@gmail.com>
2019-05-09 14:29:58 -0500

    eraser tool works

--- server/db/db.js
+++ server/db/db.js
@@ -6,6 +6,6 @@ const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
 const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
 console.log(`Opening database connection to ${dbName}`)
 
-const db = new Sequelize(dbUrl, { logging: false, operatorsAliases: false })
+const db = new Sequelize(dbUrl, { logging: false })
 
 module.exports = db
