07d5cd23ad18d2b4e73facad6efef13bd6374fe3
Brendan Fallon <brendanc.fallon@gmail.com>
2019-05-10 23:25:57 -0500

    floodfill works, but eraser now crashes shit.  pushing to origin branch to pull master to see if this is a bug from earlier

--- server/eventHandlers/paintCan.js
+++ server/eventHandlers/paintCan.js
@@ -18,7 +18,6 @@ module.exports = function paintCan(grid, x, y, selectedColor, currentColor, visi
           let c = neighbors[i]
           if (x + c.x >=0 && y + c.y >= 0 && x + c.x < grid[0].length && y + c.y < grid.length){
             if (grid[y + c.y][x + c.x] === currentColor && !visited[`${x + c.x}x${y + c.y}y`]){
-            //   grid[y + c.y][x + c.x] = selectedColor
               changeArr = changeArr.concat(paintCan(grid, x + c.x, y + c.y, selectedColor, currentColor, visited))
             }
           } 
