11f5e172cc22a8d1fe7b7a1ca247bcb346888dfd
Cassandra Terry <cassandra34terry@gmail.com>
2019-05-13 16:53:49 -0500

    homepage and navbar routing

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,11 +1,24 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { SocketContext, SpriteContext } from '../contexts';
 const constants = require('../../shared/constants');
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = (props) => {
     const [name, setName ] = useState('Untitled');
     const sprite = useContext(SpriteContext);
     const socket = useContext(SocketContext);
+    let hashString = '1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM';
+    let hashVal;
+
+
+        for (let i = 0; i < 10; i++) {
+            hashVal = ''
+            for (let j = 0; j < 12; j++) {
+                hashVal += hashString[Math.floor(Math.random() * hashString.length)];
+            }
+        }
+
+        console.log('props', props)
     useEffect(() => {
         if (socket) {
           if (sprite) {
@@ -15,14 +28,17 @@ const Navbar = () => {
       }, [sprite]);
 
     const onSpriteNameChange = (evt) => {
-        console.log('val', evt.target.value)
         setName(evt.target.value)
         socket.emit(constants.MSG.UPDATE_SPRITE_NAME, evt.target.value);
     }
     return (
         <div className="top-section-container">
             <div className="top-left">
+                <Link
+                    to= "/"
+                    style={{ textDecoration: 'none', color: 'white'}} >
                 <div className="app-title">PIXALIVE</div>
+                </Link>
             </div>
             <div className="top-middle">
                 <div className="sprite-title">
@@ -36,7 +52,11 @@ const Navbar = () => {
                 </div>
             </div>
             <div className="top-right">
+                <Link
+                    to={`/${hashVal}`}
+                    style={{ textDecoration: 'none', color: '#212121'}} >
                 <div className="top-button">Create Sprite</div>
+                </Link>
                 <div className="top-button">Export</div>
             </div>
         </div>
