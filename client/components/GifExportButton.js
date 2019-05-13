7584ffa832673012c8f72d554cf86804ac1ec38b
Jon Kurinsky <jkurinsky@gmail.com>
2019-05-13 12:20:34 -0500

    add gifshot

--- client/components/GifExportButton.js
+++ client/components/GifExportButton.js
@@ -26,21 +26,35 @@ const GifExportButton = () => {
         workers: 2,
         quality: 10,
         workerScript: 'gif.worker.js',
-        repeat: 0
+        repeat: 0,
+        width,
+        height,
+        debug: true
       });
 
-      // draw some layers
-      ctx.clearRect(0, 0, width, height);
-      const layers = sprite.frames[0].layers;
-      renderSmallCanvas(ctx, layers, width, height);
-
-      // add to gif
-      gif.addFrame(canvas, { delay: 200 });
+      // for each frame
+      sprite.frames.forEach(frame => {
+        ctx.fillStyle = '#FFFFFF';
+        ctx.fillRect(0, 0, width, height);
+        renderSmallCanvas(ctx, frame.layers, width, height);
+        const img = document.createElement('img');
+        img.src = canvas.toDataURL();
+        document.body.appendChild(img);
+        const prompt = window.prompt('test', 'test');
+        gif.addFrame(img);
+      });
 
       // when done
       gif.on('finished', blob => {
-        window.open(URL.createObjectURL(blob));
+        // destroy the canvas
         document.body.removeChild(canvas);
+
+        // create image
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(blob);
+        document.body.appendChild(img);
+
+        window.open(URL.createObjectURL(blob));
       });
 
       // render
