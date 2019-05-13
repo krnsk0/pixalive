23229f7beb38a4bae6125c87a3913037b340400b
Brendan Fallon <brendanc.fallon@gmail.com>
2019-05-13 14:23:36 -0500

    resize sprite file finished

--- server/eventHandlers/ResizeSprite.js
+++ server/eventHandlers/ResizeSprite.js
@@ -10,16 +10,20 @@ module.exports = (socket, namespacedIo, state, spriteHash, socketId) => {
         const resizePixelGrids = (grid, spriteSize) => {
           let gridEnds = []
           let numToAdd = spriteSize - grid[0].length
-          for (let i = 0; i < numToAdd; i++){
-            gridEnds.push(null)
-          }
-          grid = grid.map(row => row.concat(gridEnds))
-          let nullRows = []
-          for (let i = 0; i < spriteSize; i++){
-              nullRows.push(null)
-          }
-          for (let i = 0; i < numToAdd; i++){
-              grid.push(nullRows)
+          if (numToAdd > 0){
+            for (let i = 0; i < numToAdd; i++){
+                gridEnds.push(null)
+              }
+              grid = grid.map(row => row.concat(gridEnds))
+              let nullRows = []
+              for (let i = 0; i < spriteSize; i++){
+                  nullRows.push(null)
+              }
+              for (let i = 0; i < numToAdd; i++){
+                  grid.push(nullRows)
+              }
+          } else if (numToAdd < 0){
+              grid = grid.map(row => row.slice(0, numToAdd)).slice(0, numToAdd)
           }
           return grid
         }
