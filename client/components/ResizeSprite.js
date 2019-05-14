91821c73bf106bcc18b1868844eaaaf6a7873228
Brendan Fallon <brendanc.fallon@gmail.com>
2019-05-14 15:29:28 -0500

    got rid of button on dropdown, canvas resizes with dropdown selection

--- client/components/ResizeSprite.js
+++ client/components/ResizeSprite.js
@@ -1,37 +1,42 @@
-import React, { useState, useContext } from 'react'
-import { SocketContext } from '../contexts';
+import React, { useState, useContext, useEffect } from 'react';
+import { SocketContext, SpriteContext } from '../contexts';
 const constants = require('../../shared/constants');
 
-
-
-const  NewSpriteSize = () =>  {
+const NewSpriteSize = () => {
   const socket = useContext(SocketContext);
-  
-  const [spriteSize, setSpriteSize] = useState(16)
- 
-  
-  const handleSubmit = (evt) => {
-    evt.preventDefault()
-    if (socket) {
-      console.log("EMITTING MSG", spriteSize)
-      socket.emit(constants.MSG.RESIZE_SPRITE, spriteSize);
-    }
-  }
+  const sprite = useContext(SpriteContext);
 
+  // useEffect(() => {
+  //   if (sprite) {
+  //     setSpriteSize(sprite.frames[0].layers[0].pixels.length);
+  //   }
+  // }, [sprite]);
+
+
+  const [spriteSize, setSpriteSize] = useState();
+
+  const handleChange = evt => {
+    setSpriteSize(evt.target.value)
+    evt.preventDefault();
+    if (socket) {
+      socket.emit(constants.MSG.RESIZE_SPRITE, evt.target.value);
+      socket.emit(constants.TOOLS.SELECT_TOOL, constants.TOOLS.PEN)
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit}>
-        <select onChange={(e) => {
-          setSpriteSize(e.target.value)
-          }}>
-          <option value={16}>16x16</option>
-          <option value={32}>32x32</option>
-          <option value={48}>48x48</option>
-          <option value={64}>64x64</option>
-        </select>
-      <input type='submit' value='Resize sprite'></input>
+    <form>
+      <select
+        value={spriteSize}
+        onChange={handleChange}
+      >
+        <option value={16}>16x16</option>
+        <option value={32}>32x32</option>
+        <option value={48}>48x48</option>
+        <option value={64}>64x64</option>
+      </select>
     </form>
   );
-}
+};
 
-export default NewSpriteSize
+export default NewSpriteSize;
