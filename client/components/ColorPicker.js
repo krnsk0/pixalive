cb037494cf3c3fe10a1096be94de87d5d76dc0e5
Elizabeth Silverstein <elizabeth.a.silverstein@gmail.com>
2019-05-14 11:20:05 -0500

    H canvas click to SL canvas re-render done

--- client/components/ColorPicker.js
+++ client/components/ColorPicker.js
@@ -8,6 +8,7 @@ const ColorPicker = () => {
   const [H, setH] = useState(0);
   const [S, setS] = useState(0);
   const [L, setL] = useState(0);
+  const PIXEL_SIZE = 10;
 
   // set up canvas width & height after first mount
   useEffect(() => {
@@ -24,7 +25,6 @@ const ColorPicker = () => {
   useEffect(() => {
     const slCanvas = slCanvasRef.current;
     const ctx = slCanvas.getContext('2d');
-    const PIXEL_SIZE = 10;
     ctx.clearRect(0, 0, constants.SL_PICKER_WIDTH, constants.SL_PICKER_HEIGHT);
     for (let y = 0; y < constants.SL_PICKER_HEIGHT; y += PIXEL_SIZE) {
       for (let x = 0; x < constants.SL_PICKER_HEIGHT; x += PIXEL_SIZE) {
@@ -33,12 +33,10 @@ const ColorPicker = () => {
       }
     }
   }, [H]);
-
   // paint the H canvas just once
   useEffect(() => {
     const hCanvas = hCanvasRef.current;
     const ctx = hCanvas.getContext('2d');
-    const PIXEL_SIZE = 10;
     ctx.clearRect(0, 0, constants.H_PICKER_WIDTH, constants.H_PICKER_HEIGHT);
     for (let x = 0; x < constants.H_PICKER_WIDTH; x += PIXEL_SIZE) {
       ctx.fillStyle = `hsl(${(x / 100) * 360}, ${100}%, ${50}%, 1.0)`;
@@ -50,9 +48,33 @@ const ColorPicker = () => {
     backgroundColor: `hsl(${H}, ${S}%, ${L}%, 1.0)`
   };
 
+  //Click handler that converts x axis to H value
+  function hPicker (xLocation) {
+    //Get the ref to the hCanvas
+    const hCanvas = hCanvasRef.current;
+    let hRect = hCanvas.getBoundingClientRect()
+    let hRectLeft = hRect.left
+    let hRectRight = hRect.right
+    const rectX = xLocation - hRectLeft
+    let totalRange = hRectRight - hRectLeft
+    //Get the index of the pixel clicked
+    let pixelIndex = Math.floor((rectX / totalRange) * PIXEL_SIZE)
+    //Use the pixel index to get the return H color
+    let returnHColor = pixelIndex * 36
+    //Set the H color to require a re-render of the SL Canvas
+    setH(returnHColor)
+
+  }
+
   return (
     <div className="color-picker-container">
-      <canvas ref={hCanvasRef} className="h-picker" />
+      <canvas
+      ref={hCanvasRef}
+      onClick={e => {
+        hPicker(e.clientX)
+      }}
+       className="h-picker"
+       />
       <canvas ref={slCanvasRef} className="sl-picker" />
       <div className="selected-color-row">
         <div>Selected color</div>
